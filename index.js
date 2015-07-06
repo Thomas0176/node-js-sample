e74acce3e4b7bcd15d46d9c73dffa34de8b3e198
unknown <tvdo@SJC3-5CG5033FRK.ant.amazon.com>
2015-07-06 06:01:24 -0700

    modiciations at airport in stockholm

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
-var express = require('express')
+/**
+ *@authors: Ramon Inglesias, Thomas von der Ohe 
+ *This application uses node.js to handle Alexa Skill requests that:
+ *
+ * 
+ * Dialog model:
+ *  User: "Alexa, ask Rikel: What do you think of people that suck? "
+ *  Alexa: "They suck. 
+ *  User: "Are you sure?"
+ *  Alexa: "Yes, absolutely."
+ */
+
+var NAMES_LIST = [
+	{firstName:"Rikelme", lastName:"Rikel", country: "Spain", age: 27, profession: "Student", favoriteSport: "soccer", hasMoustache: "No", eyeColor:"blue"},
+	{firstName:"Ramonster", lastName:"Iglesias", country: "Mexico", age: 26, profession: "Student", favoriteSport: "soccer", hasMoustache: "Yes, but barely.", eyeColor:"blue"},
+	{firstName:"Thomasito", lastName:"von der Ohe", country: "Germany", age: 27, profession: "Package deliverer", favoriteSport: "soccer", hasMoustache: "Yes, an impressive one.", eyeColor:"blue"},
+ ];
+
+
+var express = require('express');
 var app = express();
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
 
-// This line is from the Node.js HTTPS documentation.
+
+/**
+ * The EchoJSONParser prototype and helper functions
+ */
+var EchoJSONParser = require('echoJSONParser');
+
+var RikelSkill = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+
+// This line is from the Node.js HTTPS documentation
 var options = {
   key: fs.readFileSync('keys/private-key.pem'),
   cert: fs.readFileSync('keys/certificate.pem')
 };
 
-// app.set('port', (process.env.PORT || 5000))
-// app.use(express.static(__dirname + '/public'))
 
-http.createServer(app).listen(6000,  function() {
-  console.log("Node app is running at localhost:" + 6000)
-})
-
-https.createServer(options, app).listen(5000, function() {
-  console.log("Node app is running at localhost:" + 5000)
-})
+http.createServer(app).listen(8080,  function() {
+  console.log("Node app is running at localhost:" + 8080)
+});
 
 
+//Used to use encryption
+// https.createServer(options, app).listen(5000, function() {
+//   console.log("Node app is running at localhost:" + 5000)
+// })
 
-
-var EchoJSONParser = require('echoJSONParser')
 
 app.get('/', function(request, response) {
-  response.send("Hello wordd")
-})
+  response.send("Hello world")
+});
 
 
 app.post('/', function(request, response){
-
-
 	var echoJSONParser = new EchoJSONParser('request')
 
 	var responseString = echoJSONParser.createResponse();
